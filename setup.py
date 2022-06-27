8e6c8d51111ac8357b04f60701ec179f65a39277
Rami <rhadjahmed@hadooc.com>
2022-06-27 11:21:58 +0100

    Add ummalqura3

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='ummalqura3',
+   version='3.0',
+   description='ummalqura3',
+   author=' ',
+   author_email=' ',
+   packages=['ummalqura3']
+)
+
---
+++ ummalqura3/__init__.py
@@ -0,0 +1,3 @@
+from . import hijri
+from . import hijri_date
+from . import ummalqura_arrray
